6dff99b7ddbdf97cf68cd8a336941c0b3bdc5622
BryantGG77g <brayespro@gmail.com>
2025-10-18 22:42:46 -0500

    Logo error

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,7 +4,7 @@ export const Header = ({ filtro }) => {
   return (
     <header className="flex flex-col justify-center items-center gap-4 w-full py-10 px-4 bg-gradient-to-br from-cyan-300 via-blue-500 to-indigo-300
  text-center">
-      <img className="max-w-[300px]" src="..\src\assets\logo.png" alt="PokéDex logo" />
+      <img className="max-w-[300px]" src="src/assets/logo.png" alt="PokéDex logo" />
 
       <p className="text-black lg:text-2xl md:text-xl sm:text-lg">
         Descubre y explora todos los Pokémon de la primera generación
